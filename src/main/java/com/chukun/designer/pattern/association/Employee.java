b9edde5f56507a141bb2d824d288a03926299b95
chukun <492479702@qq.com>
2020-04-19 22:54:24 +0800

    组合设计模式示例

---
+++ src/main/java/com/chukun/designer/pattern/association/Employee.java
@@ -0,0 +1,17 @@
+package com.chukun.designer.pattern.association;
+
+/**
+ * @author chukun
+ *  计算员工的薪水
+ */
+public class Employee extends HumanResource {
+
+    public Employee(long id,double salary) {
+        super(id);
+        this.salary = salary;
+    }
+    @Override
+    public double calculateSalary() {
+        return salary;
+    }
+}
